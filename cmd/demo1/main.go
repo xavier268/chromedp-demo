0c7c39b27cd3875fadd57b9a3f0e68930c074554
xavier <xavier@fed29onDellXPS>
2019-05-22 19:58:52 +0200

    Work in progress

---
+++ cmd/demo1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+
+	dir, err := ioutil.TempDir("", "xavier-chromedp-demo")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	opts := []chromedp.ExecAllocatorOption{
+		chromedp.NoFirstRun,
+		chromedp.NoDefaultBrowserCheck,
+		chromedp.Headless,
+		chromedp.DisableGPU,
+		chromedp.UserDataDir(dir),
+	}
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// also set up a custom logger
+	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	// ensure that the browser process is started
+	if err := chromedp.Run(taskCtx); err != nil {
+		panic(err)
+	}
+
+	path := filepath.Join(dir, "DevToolsActivePort")
+	bs, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	lines := bytes.Split(bs, []byte("\n"))
+	fmt.Printf("DevToolsActivePort has %d lines\n", len(lines))
+
+	// run task list
+	var res string
+	err = chromedp.Run(taskCtx,
+		chromedp.Navigate(`https://golang.org/pkg/time/`),
+		chromedp.Text(`#pkg-overview`, &res, chromedp.NodeVisible, chromedp.ByID),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(strings.TrimSpace(res))
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module github.com/xavier268/chromedp-demo
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20190511222037-764a6f24cacb // indirect
+	github.com/chromedp/chromedp v0.3.0
+	github.com/gobwas/ws v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20190522044717-8097e1b27ff5 // indirect
+)
