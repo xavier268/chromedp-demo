934209f904ce01d8998810ef180191601b821a86
xavier <xavier@fed29onDellXPS>
2019-05-25 20:07:16 +0200

    updated demo1

--- cmd/demo1/main.go
+++ cmd/demo1/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
+	"time"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
+)
+
+const (
+	myurl = "https://news.google.com"
 )
 
 func main() {
@@ -24,40 +27,99 @@ func main() {
 	opts := []chromedp.ExecAllocatorOption{
 		chromedp.NoFirstRun,
 		chromedp.NoDefaultBrowserCheck,
-		chromedp.Headless,
+		//chromedp.Headless,			  // Operate in headless mode
+		chromedp.Flag("headless", false), // Display what's going on ...
 		chromedp.DisableGPU,
 		chromedp.UserDataDir(dir),
 	}
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	// Create amllocator context
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
-	// also set up a custom logger
-	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	// Use allocator to get a browser context (MANDATORY - you cannot use an allocator ctx directly )
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	// Set timeout, just in case ...
+	ctx, cancel = context.WithTimeout(ctx, 3*time.Minute)
+	fmt.Println(time.Now(), " Timout started for 3 minutes")
 	defer cancel()
 
 	// ensure that the browser process is started
-	if err := chromedp.Run(taskCtx); err != nil {
+	if err := chromedp.Run(ctx); err != nil {
 		panic(err)
 	}
 
-	path := filepath.Join(dir, "DevToolsActivePort")
-	bs, err := ioutil.ReadFile(path)
-	if err != nil {
+	// Actual processing starts here
+
+	getSelectedHeadlines(ctx)
+	getAllHeadlines(ctx)
+}
+
+func checkBroserStarted(ctx context.Context) {
+	// ensure that the browser process is started
+	if err := chromedp.Run(ctx); err != nil {
 		panic(err)
 	}
-	lines := bytes.Split(bs, []byte("\n"))
-	fmt.Printf("DevToolsActivePort has %d lines\n", len(lines))
+}
 
-	// run task list
+func getSelectedHeadlines(ctx context.Context) {
+
 	var res string
-	err = chromedp.Run(taskCtx,
-		chromedp.Navigate(`https://golang.org/pkg/time/`),
-		chromedp.Text(`#pkg-overview`, &res, chromedp.NodeVisible, chromedp.ByID),
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(myurl),
+		chromedp.Title(&res),
+		myPrintAction{&res},
+		// Dump outerHtml
+		chromedp.OuterHTML("span ~ a", &res),
+		myPrintAction{&res},
+		// Search by css selector
+		chromedp.Text("article h3", &res),
+		myPrintAction{&res},
+		// search by css selector
+		chromedp.Text("article ~ div h4", &res),
+		myPrintAction{&res},
+		// Print text covering multiple tags
+		chromedp.Text("article ~ div", &res),
+		myPrintAction{&res},
+		//Test xpath
+		chromedp.Text("//h4//a", &res),
+		myPrintAction{&res},
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	log.Println(strings.TrimSpace(res))
+func getAllHeadlines(ctx context.Context) {
+
+	var lnodes []*cdp.Node
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(myurl),
+		chromedp.Nodes("//div[15]//h4/a", &lnodes),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("\nlnodes : \t", lnodes)
+	for i := range lnodes {
+		fmt.Println("\n=================\tNode N° ", i, "\t====================")
+		fmt.Println("href     \t", lnodes[i].AttributeValue("href"))
+		fmt.Println("xpath    \t", lnodes[i].FullXPath())
+		fmt.Println("nodevalue\t", lnodes[i].NodeValue)
+		fmt.Println("nodename \t", lnodes[i].NodeName)
+		fmt.Println()
+	}
+
+}
+
+// Define a printable action string
+type myPrintAction struct {
+	*string
+}
+
+func (s myPrintAction) Do(ctx context.Context) error {
+	fmt.Printf("my log :\t%s\n", *(s.string))
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/xavier268/chromedp-demo
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20190511222037-764a6f24cacb // indirect
+	github.com/chromedp/cdproto v0.0.0-20190511222037-764a6f24cacb
 	github.com/chromedp/chromedp v0.3.0
-	github.com/gobwas/ws v1.0.1 // indirect
-	golang.org/x/sys v0.0.0-20190522044717-8097e1b27ff5 // indirect
 )
