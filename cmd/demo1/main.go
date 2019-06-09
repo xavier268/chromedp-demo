12c3c76fad184676f5e727908b6800114e351035
xavier <xavier@fed29onDellXPS>
2019-06-09 12:12:47 +0200

    refactoring

--- cmd/demo1/main.go
+++ cmd/demo1/main.go
@@ -3,20 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 	"time"
 
-	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
-const (
-	myurl = "https://news.google.com"
-)
-
 func main() {
+
+	const (
+		myurl = "https://news.google.com"
+	)
 
 	// Get a default (visible) allocator
 	ctx, cancel := getVisibleAllocator()
@@ -36,102 +32,6 @@ func main() {
 
 	// Actual processing done here
 
-	//getSelectedHeadlines(ctx)
-	getAllHeadlines(ctx)
-}
-
-// Provides an allocator with default option, that opens a visible browser
-// cookies etc are shared in a common dir that gets erased at the end
-func getVisibleAllocator() (context.Context, context.CancelFunc) {
-	dir, err := ioutil.TempDir("", "xavier-chromedp-demo")
-	if err != nil {
-		panic(err)
-	}
-
-	opts := []chromedp.ExecAllocatorOption{
-		chromedp.NoFirstRun,
-		chromedp.NoDefaultBrowserCheck,
-		//chromedp.Headless,			  // Operate in headless mode
-		chromedp.Flag("headless", false), // Display what's going on ...
-		chromedp.DisableGPU,
-		chromedp.UserDataDir(dir),
-	}
-
-	// Create allocator context
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-
-	cancel2 := func() {
-		cancel()
-		os.RemoveAll(dir)
-	}
-
-	return ctx, cancel2
-}
-
-func checkBrowserStarted(ctx context.Context) {
-	// ensure that the browser process is started
-	if err := chromedp.Run(ctx); err != nil {
-		panic(err)
-	}
-}
-
-func getSelectedHeadlines(ctx context.Context) {
-
-	var res string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(myurl),
-		chromedp.Title(&res),
-		myPrintAction{&res},
-		// Dump outerHtml
-		chromedp.OuterHTML("span ~ a", &res),
-		myPrintAction{&res},
-		// Search by css selector
-		chromedp.Text("article h3", &res),
-		myPrintAction{&res},
-		// search by css selector
-		chromedp.Text("article ~ div h4", &res),
-		myPrintAction{&res},
-		// Print text covering multiple tags
-		chromedp.Text("article ~ div", &res),
-		myPrintAction{&res},
-		//Test xpath
-		chromedp.Text("//h4//a", &res),
-		myPrintAction{&res},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getAllHeadlines(ctx context.Context) {
-
-	var lnodes []*cdp.Node
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(myurl),
-		chromedp.Nodes("//div//h4/a", &lnodes),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("\nlnodes : \t", lnodes)
-	for i := range lnodes {
-		fmt.Println("\n=================\tNode N° ", i, "\t====================")
-		fmt.Println("href     \t", lnodes[i].AttributeValue("href"))
-		fmt.Println("xpath    \t", lnodes[i].FullXPath())
-		fmt.Println("nodevalue\t", lnodes[i].NodeValue)
-		fmt.Println("nodename \t", lnodes[i].NodeName)
-		fmt.Println()
-	}
-	fmt.Println("Done - getAllHeadlines")
-
-}
-
-// Define a printable action string
-type myPrintAction struct {
-	*string
-}
-
-func (s myPrintAction) Do(ctx context.Context) error {
-	fmt.Printf("my log :\t%s\n", *(s.string))
-	return nil
+	getSelectedHeadlines(ctx, myurl)
+	getAllHeadlines(ctx, myurl)
 }
